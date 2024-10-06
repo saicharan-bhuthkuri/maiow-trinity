fc602088187a2edf3af468f1981c014a10f3f34c
sai <saicharanbhuthkuri9@gmail.com>
2024-10-06 22:52:14 +0530

    first commit

---
+++ prompt/prompt.js
@@ -0,0 +1,10 @@
+document.getElementById('design-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const designIdea = document.getElementById('design-input').value;
+    
+    if (designIdea) {
+        alert('Generating web design for: ' + designIdea);
+    } else {
+        alert('Please enter a web design idea.');
+    }
+});
